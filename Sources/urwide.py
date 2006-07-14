05b02a83dac753485f236b35e7c2f1c9546ea42a
Sébastien Pierre <sebastien@xprima.com>
2006-07-14 15:43:59 -0400

    Added basic URWID event loop binding. The test is still not yet functional, but I am going there...

--- Sources/urwide.py
+++ Sources/urwide.py
@@ -1,5 +1,27 @@
+#!/usr/bin/env python
+# Encoding: iso-8859-1
+# vim: tw=80 ts=4 sw=4 noet fenc=latin-1
+# -----------------------------------------------------------------------------
+# Project   : URWIDE - Extended URWID
+# -----------------------------------------------------------------------------
+# Author    : Sébastien Pierre                           <sebastien@xprima.com>
+# License   : Lesser GNU Public License  http://www.gnu.org/licenses/lgpl.html>
+# -----------------------------------------------------------------------------
+# Creation  : 14-Jul-2006
+# Last mod  : 14-Jul-2006
+# -----------------------------------------------------------------------------
+
 import string, re
-import urwid
+import urwid, urwid.curses_display
+
+__version__ = "0.0.2"
+__doc__ = """\
+URWIDE provides a nice wrapper around the awesome urwid Python library. It
+enables the creation of complexe console user-interfaces, using a simple syntax. 
+
+URWIDE provides extensions to support events, tooltips, dialogs as well as other
+goodies.
+"""
 
 COLORS =  {
 	# Colors
@@ -14,39 +36,11 @@ COLORS =  {
 	"_" : "default"
 }
 
-def palette( data ):
-	res = []
-	for line in data.split("\n"):
-		if not line.strip(): continue
-		line = line.replace("\t", " ").replace("  ", " ")
-		name, attributes = map(string.strip, line.split(":"))
-		res_line = [name]
-		for attribute in attributes.split(","):
-			attribute = attribute.strip().upper()
-			color     = COLORS.get(attribute)
-			if not color: raise Exception("Unsupported color: " + attribute)
-			res_line.append(color)
-		if not len(res_line) == 4:
-			raise Exception("Expected NAME: FOREGROUND BACKGROUND FONT")
-		res.append(tuple(res_line))
-	return res
-
-
-palette("""
-	background    : DG,  _, SO
-	header        : WH, DG, BL
-	footer        : LG,  _, SO
-	info          : WH, LG, BL
-	shade         : DC, LG, BL
-""")
-
-
 CLASSES = {
 	"Edt": urwid.Edit,
 	"Ple": urwid.Pile,
 	"GFl": urwid.GridFlow,
 }
-
 
 class UISyntaxError(Exception): pass
 class UI:
@@ -61,29 +55,21 @@ class UI:
 	EMPTY = urwid.Text("")
 	NOP   = lambda x:x
 
-	def __init__( self, text ):
+	def __init__( self, palette, ui ):
 		"""Creates a new user interface object from the given text
 		description."""
-		self.parse(text)
-		self.content = None
-		self.stack = None
-		self.currentLine = None
+		self._content     = None
+		self._stack       = None
+		self._currentLine = None
+		self._ui          = None
+		self._palette     = self.parsePalette(palette)
+		self._frame       = urwid.Frame(
+			urwid.ListBox(
+				self.parse(ui)
+			)
+		)
+		self._topwidget   = self._frame
 
-	def parse( self, text ):
-		"""Parses the given text and initializes this user interface object."""
-		self.content = []
-		self.stack   = []
-		self.currentLine = 0
-		for line in text.split("\n"):
-			line = line.strip()
-			try:
-				self._parseLine(line)
-			except SyntaxError, e:
-				print "ERROR:", e
-				print "at line %d: %s" % (self.currentLine, repr(line))
-				print e
-				print
-			self.currentLine += 1
 
 	# EVENT HANDLERS
 	# -------------------------------------------------------------------------
@@ -91,27 +77,96 @@ class UI:
 	def _onPress( self, button ):
 		pass
 
+	def _onKeyPress( self, key ):
+		focused_widget = self.getFocused()
+		if focused_widget:
+			pass
+		else:
+			self._topwidget.keypress(self._currentSize, key)
+
+	# UI STATE MANAGEMENT
+	# -------------------------------------------------------------------------
+
+	def getFocused( self ):
+		return None
+
+	# URWID EVENT-LOOP
+	# -------------------------------------------------------------------------
+
+	def main( self ):
+		self._ui = urwid.curses_display.Screen()
+		if self._palette: self._ui.register_palette(self._palette)
+		self._ui.run_wrapper( self.run )
+
+	def run( self ):
+		self._currentSize = self._ui.get_cols_rows()
+		while True:
+			self.loop()
+
+	def loop( self ):
+		self.draw()
+		keys = self._ui.get_input()
+		# We handle keys
+		for key in keys:
+			if key == "window resize":
+				self._currentSize = self._ui.get_cols_rows()
+			else:
+				self._onKeyPress(key)
+	
+	def draw( self ):
+		canvas = self._topwidget.render( self._currentSize, focus=True )
+		self._ui.draw_screen( self._currentSize, canvas )
+
 	# PARSING WIDGETS STACK MANAGEMENT
 	# -------------------------------------------------------------------------
 
 	def _add( self, widget ):
 		# Piles cannot be created with [] as content, so we fill them with the
 		# EMPTY widget, which is replaced whenever we add something
-		if self.content == [self.EMPTY]: self.content[0] == widget
-		self.content.append(widget)
+		if self._content == [self.EMPTY]: self._content[0] == widget
+		self._content.append(widget)
 
 	def _push( self, endCallback, ui=None, args=(), kwargs={} ):
-		self.stack.append((self.content, endCallback, ui, args, kwargs))
-		self.content = []
-		return self.content
+		self._stack.append((self._content, endCallback, ui, args, kwargs))
+		self._content = []
+		return self._content
 
 	def _pop( self ):
-		previous_content = self.content
-		self.content, end_callback, end_ui, end_args, end_kwargs = self.stack.pop()
+		previous_content = self._content
+		self._content, end_callback, end_ui, end_args, end_kwargs = self._stack.pop()
 		return previous_content, end_callback, end_ui, end_args, end_kwargs
 
 	# GENERIC PARSING METHODS
 	# -------------------------------------------------------------------------
+
+	def parse( self, text ):
+		"""Parses the given text and initializes this user interface object."""
+		self._content = []
+		self._stack   = []
+		self._currentLine = 0
+		for line in text.split("\n"):
+			line = line.strip()
+			self._parseLine(line)
+			self._currentLine += 1
+		return self._content
+
+	@staticmethod
+	def parsePalette( data ):
+		res = []
+		for line in data.split("\n"):
+			if not line.strip(): continue
+			line = line.replace("\t", " ").replace("  ", " ")
+			name, attributes = map(string.strip, line.split(":"))
+			res_line = [name]
+			for attribute in attributes.split(","):
+				attribute = attribute.strip().upper()
+				color     = COLORS.get(attribute)
+				if not color: raise UISyntaxError("Unsupported color: " + attribute)
+				res_line.append(color)
+			if not len(res_line) == 4:
+				raise UISyntaxError("Expected NAME: FOREGROUND BACKGROUND FONT")
+			res.append(tuple(res_line))
+		return res
 
 	RE_LINE = re.compile("^\s*(...)\s*")
 	def _parseLine( self, line ):
@@ -200,6 +255,15 @@ class UI:
 	RE_EDT = re.compile("([^\[]*)\[([^\]]+)\]")
 	def _parseEdt( self, data ):
 		match = self.RE_EDT.match(data)
+		data  = data[match.end():]
+		label, text = match.groups()
+		ui, args, kwargs = self._parseAttributes(data)
+		if label:
+			self._add(self._createWidget(urwid.Edit, '', text,
+			ui=ui, args=args, kwargs=kwargs))
+		else:
+			self._add(self._createWidget(urwid.Edit, '', text,
+			ui=ui, args=args, kwargs=kwargs))
 
 	def _parsePle( self, data ):
 		def end( content, ui=None, **kwargs ):
@@ -211,7 +275,7 @@ class UI:
 	def _parseGFl( self, data ):
 		def end( content, ui=None, **kwargs ):
 			max_width = 0
-			for widget in self.content:
+			for widget in self._content:
 				if hasattr(widget, "get_text"):
 					max_width = max(len(widget.get_text()), max_width)
 				if hasattr(widget, "get_label"):
@@ -223,18 +287,25 @@ class UI:
 		if data.strip(): raise UISyntaxError("End takes no argument: " + repr(data))
 		# We get the end callback that will instanciate the widget and add it to
 		# the content.
-		if not self.stack: raise SyntaxError("End called without container widget")
+		if not self._stack: raise SyntaxError("End called without container widget")
 		end_content, end_callback, end_ui, end_args, end_kwargs = self._pop()
 		end_callback(end_content, end_ui, *end_args, **end_kwargs)
 
 ui = UI("""
+background    : DG,  _, SO
+header        : WH, LG, BL
+footer        : LG,  _, SO
+info          : WH, LG, BL
+shade         : DC, LG, BL
+""",
+"""
 ::: @shade
 
 Edt  State         [Project state]  #edit_state
-Edt  Commit Type   [Project state]  #edit_type
+Edt  Commit Type   [Commitl type]   #edit_type
 Edt  Summary       [Summary]        #edit_summary
 ---
-Edt  [Description] multiline=True   #edit_desc
+Edt  [Description]  #edit_desc multiline=True
 ===
 Txt  Changes to commit  
 ---
@@ -245,4 +316,7 @@ Btn [Cancel]                         #btn_cancel
 Btn [Save]                           #btn_save
 Btn [Commit]                         #btn_commit
 End
-""")
+"""
+)
+
+ui.main()
